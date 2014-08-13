868834483d7d6d04e801ca06bacdf6261b185811
chris ward <closet13@gmail.com>
2014-08-13 09:16:49 -0400

    yet another submit change test

--- SimpleCalc/Program.cs
+++ SimpleCalc/Program.cs
@@ -11,7 +11,7 @@ namespace SimpleCalc
     {
         static void Main()
         {
-            // test submit to git hub
+            // test submit to git hub ..... ......
             Console.WriteLine("First Integer: ");
             String firstint;
             firstint = Console.ReadLine();
