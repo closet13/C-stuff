fe4ba39f8898bec9b3416c14f879e6d3762db106
chris ward <closet13@gmail.com>
2014-08-13 08:44:02 -0400

    test program

---
+++ SimpleCalc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalc
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("First Integer: ");
+            String firstint;
+            firstint = Console.ReadLine();
+            int parseInt1 = int.Parse(firstint);
+
+            Console.WriteLine("Second Integer: ");
+            String secondint;
+            secondint = Console.ReadLine();
+            int parseInt2 = int.Parse(secondint);
+
+            int result = parseInt1 + parseInt2;
+            Console.WriteLine("The result is: " + result);
+            Console.ReadLine();
+
+        }
+    }
+}
